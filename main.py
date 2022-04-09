2fe4d7dca533e2999dc1146367f6a86a64ef4561
saheedRx <89293737+saheedRx@users.noreply.github.com>
2022-04-09 12:29:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,8 +55,7 @@ async def startprivate(client, message):
             ]
         ]
     )
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nI'm a simple Telegram bot that can broadcast messages and media to the bot subscribers. Made by @NACBOTS.\n\n 🎚 use /settings"
-    await message.reply_text(welcomed, reply_markup=joinButton)
+    welcomed =
     raise StopPropagation
 
 
